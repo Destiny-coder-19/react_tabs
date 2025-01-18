0eb5e5e2741606d9609d7a6e6dcf0733d79dbac5
Destiny-coder-19 <singhaakash1912@gmail.com>
2025-01-19 03:14:36 +0530

    commit basic tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import CoreConcept from "./Components/CoreConcept/CoreConcept.jsx";
+import Header from "./Components/Header/header.jsx";
+import TabButton from "./Components/TabButton/TabButton.jsx";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tabContent, setTabContent] = useState();
+
+  const handleSelect = (name) => {
+    setTabContent(name);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((item, i) => {
+              return (
+                <CoreConcept
+                  key={i + item.title}
+                  image={item.image}
+                  title={item.title}
+                  description={item.description}
+                />
+              );
+            })}
+          </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton
+              isSelected={tabContent === "components"}
+              onSelect={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "jsx"}
+              onSelect={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "props"}
+              onSelect={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={tabContent === "state"}
+              onSelect={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </menu>
+          <div id="tab-content">
+            {!tabContent ? (
+              "Please Click A Button"
+            ) : (
+              <>
+                <h3>{EXAMPLES[tabContent].title}</h3>
+                <p>{EXAMPLES[tabContent].description}</p>
+                <pre>{EXAMPLES[tabContent].code}</pre>
+              </>
+            )}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
